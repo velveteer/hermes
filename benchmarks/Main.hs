682bbf2d9c31452d94c9d9bb07c732d2256534e2
Josh Miller <notjoshmiller@gmail.com>
2021-10-18 00:56:07 -0500

    update benches

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE DeriveAnyClass        #-}
-{-# LANGUAGE DeriveGeneric         #-}
-{-# LANGUAGE DerivingStrategies    #-}
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE DuplicateRecordFields      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
 
 module Main where
 
@@ -28,19 +29,29 @@ setupSIMD input = do
   pure (p, d, i)
 
 main :: IO ()
-main = do
-  defaultMain
-    [ withResource (BS.readFile "benchmarks/persons.json") (const $ pure ()) $ \input ->
-        bgroup "Decode 12MB Array of Objects"
-        [ bench "Aeson Decode Strict" $
-            nfIO ((Aeson.decode' . BSL.fromStrict <$> input) :: IO (Maybe [Person]))
-        , bench "Aeson Decode Lazy" $
-            nfIO ((Aeson.decode . BSL.fromStrict <$> input) :: IO (Maybe [Person]))
-        , withResource (setupSIMD input) (const $ pure ()) $ \ptrs ->
-          bench "SIMD Decode" $
-            nfIO (do { (p, d, i) <- ptrs; decodeWith p d i :: IO [Person] })
-        ]
+main = defaultMain [
+  withResource (BS.readFile "benchmarks/persons.json") (const $ pure ()) $ \input ->
+    bgroup "Decode 12MB Array of Objects"
+    [ bgroup "Ordered Keys"
+      [ withResource (setupSIMD input) (const $ pure ()) $ \ptrs ->
+        bench "SIMD Decode" $
+          nfIO (do { (p, d, i) <- ptrs; decodeWith p d i :: IO [Person] })
+      , bench "Aeson Decode Strict" $
+          nfIO ((Aeson.decode' . BSL.fromStrict <$> input) :: IO (Maybe [Person]))
+      , bench "Aeson Decode Lazy" $
+          nfIO ((Aeson.decode . BSL.fromStrict <$> input) :: IO (Maybe [Person]))
+      ]
+    , bgroup "Unordered Keys"
+      [ withResource (setupSIMD input) (const $ pure ()) $ \ptrs ->
+        bench "SIMD Decode" $
+          nfIO (do { (p, d, i) <- ptrs; decodeWith p d i :: IO [PersonUnordered] })
+      , bench "Aeson Decode Strict" $
+          nfIO ((Aeson.decode' . BSL.fromStrict <$> input) :: IO (Maybe [PersonUnordered]))
+      , bench "Aeson Decode Lazy" $
+          nfIO ((Aeson.decode . BSL.fromStrict <$> input) :: IO (Maybe [PersonUnordered]))
+      ]
     ]
+  ]
 
 data Person =
   Person
@@ -49,7 +60,7 @@ data Person =
     , guid          :: Text
     , isActive      :: Bool
     , balance       :: Text
-    , picture       :: Text
+    , picture       :: Maybe Text
     , age           :: Int
     , eyeColor      :: Text
     , name          :: Text
@@ -64,7 +75,7 @@ data Person =
     , longitude     :: Double
     , tags          :: [Text]
     , friends       :: [Friend]
-    , greeting      :: Text
+    , greeting      :: Maybe Text
     , favoriteFruit :: Text
     } deriving (Show, Generic, NFData, Aeson.FromJSON)
 
@@ -105,3 +116,55 @@ instance FromJSON Person where
       <*> obj .:> "friends"
       <*> obj .:> "greeting"
       <*> obj .:> "favoriteFruit"
+
+data PersonUnordered =
+  PersonUnordered
+    { favoriteFruit :: Text
+    , isActive      :: Bool
+    , longitude     :: Double
+    , balance       :: Text
+    , email         :: Text
+    , latitude      :: Double
+    , age           :: Int
+    , eyeColor      :: Text
+    , index         :: Int
+    , guid          :: Text
+    , name          :: Text
+    , greeting      :: Text
+    , company       :: Text
+    , picture       :: Text
+    , phone         :: Text
+    , _id           :: Text
+    , address       :: Text
+    , about         :: Text
+    , gender        :: Text
+    , registered    :: Text
+    , tags          :: [Text]
+    , friends       :: [Friend]
+    } deriving (Show, Generic, NFData, Aeson.FromJSON)
+
+instance FromJSON PersonUnordered where
+  parseJSON valPtr = withObject valPtr $ \obj ->
+    PersonUnordered
+      <$> obj .: "favoriteFruit"
+      <*> obj .: "isActive"
+      <*> obj .: "longitude"
+      <*> obj .: "balance"
+      <*> obj .: "email"
+      <*> obj .: "latitude"
+      <*> obj .: "age"
+      <*> obj .: "eyeColor"
+      <*> obj .: "index"
+      <*> obj .: "guid"
+      <*> obj .: "name"
+      <*> obj .: "greeting"
+      <*> obj .: "company"
+      <*> obj .: "picture"
+      <*> obj .: "phone"
+      <*> obj .: "_id"
+      <*> obj .: "address"
+      <*> obj .: "about"
+      <*> obj .: "gender"
+      <*> obj .: "registered"
+      <*> obj .: "tags"
+      <*> obj .: "friends"
