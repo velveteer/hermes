e8e1c93fd03717b5a4d86567bb1886e2df2c300d
Josh Miller <notjoshmiller@gmail.com>
2021-10-21 12:41:36 -0500

    add waargonaut to benches

--- hermesbench/src/Main.hs
+++ hermesbench/src/Main.hs
@@ -7,13 +7,17 @@
 module Main where
 
 import           Control.DeepSeq       (NFData)
+import           Control.Monad         (void)
 import qualified Data.Aeson            as Aeson
 import qualified Data.ByteString       as BS
 import           Data.Functor.Identity (Identity(..))
+import           Data.Scientific       (toRealFloat)
 import           Data.Text             (Text)
 import           GHC.Generics          (Generic)
 import           Test.Tasty            (withResource)
 import           Test.Tasty.Bench
+import qualified Waargonaut.Decode     as D
+import qualified Waargonaut.Attoparsec as WA
 
 import           Data.Hermes
 
@@ -31,6 +35,8 @@ main = defaultMain
           nfIO ((Aeson.decodeStrict <$> input) :: IO (Maybe [Person]))
       , bench "Aeson Decode Strict" $
           nfIO ((Aeson.decodeStrict' <$> input) :: IO (Maybe [Person]))
+      , bench "Waargonaut Decode Attoparsec" $
+          nfIO (void $ WA.decodeAttoparsecByteString (D.list personDecoder) =<< input)
       ]
     , bgroup "Unordered Keys"
       [ bench "Hermes Decode" $
@@ -41,6 +47,8 @@ main = defaultMain
           nfIO ((Aeson.decodeStrict <$> input) :: IO (Maybe [PersonUnordered]))
       , bench "Aeson Decode Strict" $
           nfIO ((Aeson.decodeStrict' <$> input) :: IO (Maybe [PersonUnordered]))
+      , bench "Waargonaut Decode Attoparsec" $
+          nfIO (void $ WA.decodeAttoparsecByteString (D.list personUnorderedDecoder) =<< input)
       ]
     ]
   , withResource (BS.readFile "json/twitter100.json") (const $ pure ()) $ \input ->
@@ -54,6 +62,8 @@ main = defaultMain
         nfIO ((Aeson.decodeStrict <$> input) :: IO (Maybe Twitter))
     , bench "Aeson Decode Strict" $
         nfIO ((Aeson.decodeStrict' <$> input) :: IO (Maybe Twitter))
+    , bench "Waargonaut Decode Attoparsec" $
+        nfIO (void $ WA.decodeAttoparsecByteString twitterDecoder =<< input)
     ]
   ]
 
@@ -114,6 +124,38 @@ decodeFriend = withObject $ \obj ->
   Friend
     <$> atOrderedKey "id" int obj
     <*> atOrderedKey "name" text obj
+
+personDecoder :: Monad f => D.Decoder f Person
+personDecoder =
+  Person
+    <$> D.atKey "_id" D.text
+    <*> D.atKey "index" D.int
+    <*> D.atKey "guid" D.text
+    <*> D.atKey "isActive" D.bool
+    <*> D.atKey "balance" D.text
+    <*> D.atKey "picture" (D.maybeOrNull D.text)
+    <*> D.atKey "age" D.int
+    <*> D.atKey "eyeColor" D.text
+    <*> D.atKey "name" D.text
+    <*> D.atKey "gender" D.text
+    <*> D.atKey "company" D.text
+    <*> D.atKey "email" D.text
+    <*> (Identity <$> D.atKey "phone" D.text)
+    <*> D.atKey "address" D.text
+    <*> D.atKey "about" D.text
+    <*> D.atKey "registered" D.text
+    <*> (toRealFloat <$> D.atKey "latitude" D.scientific)
+    <*> (toRealFloat <$> D.atKey "longitude" D.scientific)
+    <*> D.atKey "tags" (D.list D.text)
+    <*> (Identity <$> D.atKey "friends" (D.list friendDecoder))
+    <*> D.atKey "greeting" (D.maybeOrNull D.text)
+    <*> D.atKey "favoriteFruit" D.text
+
+friendDecoder :: Monad f => D.Decoder f Friend
+friendDecoder =
+  Friend
+    <$> D.atKey "id" D.int
+    <*> D.atKey "name" D.text
 
 data Friend =
   Friend
@@ -207,6 +249,33 @@ decodePersonUnordered = withObject $ \obj ->
     <*> atKey "tags" (list text) obj
     <*> atKey "friends" (list decodeFriend) obj
 
+personUnorderedDecoder :: Monad f => D.Decoder f PersonUnordered
+personUnorderedDecoder =
+  PersonUnordered
+    <$> D.atKey "favoriteFruit" D.text
+    <*> D.atKey "isActive" D.bool
+    <*> (toRealFloat <$> D.atKey "longitude" D.scientific)
+    <*> D.atKey "balance" D.text
+    <*> D.atKey "email" D.text
+    <*> (toRealFloat <$> D.atKey "latitude" D.scientific)
+    <*> D.atKey "age" D.int
+    <*> D.atKey "eyeColor" D.text
+    <*> D.atKey "index" D.int
+    <*> D.atKey "guid" D.text
+    <*> D.atKey "name" D.text
+    <*> D.atKey "greeting" D.text
+    <*> D.atKeyOptional "nonexistent" D.text
+    <*> D.atKey "company" D.text
+    <*> D.atKey "picture" D.text
+    <*> D.atKey "phone" D.text
+    <*> D.atKey "_id" D.text
+    <*> D.atKey "address" D.text
+    <*> D.atKey "about" D.text
+    <*> D.atKey "gender" D.text
+    <*> D.atKey "registered" D.text
+    <*> D.atKey "tags" (D.list D.text)
+    <*> D.atKey "friends" (D.list friendDecoder)
+
 data Twitter =
   Twitter
     { statuses :: [Status]
@@ -235,12 +304,12 @@ decodeTwitter = withObject $ \obj ->
 
 decodeStatus :: Value -> IO Status
 decodeStatus = withObject $ \obj -> do
-    u <- atKey "user" decodeUser obj
-    md <- atKey "metadata" pure obj
-    flip withObject md $ \m -> do
-      result <- atKey "result_type" text m
-      iso <- atKey "iso_language_code" text m
-      pure $ Status u result iso
+  u <- atKey "user" decodeUser obj
+  md <- atKey "metadata" pure obj
+  flip withObject md $ \m -> do
+    result <- atKey "result_type" text m
+    iso <- atKey "iso_language_code" text m
+    pure $ Status u result iso
 
 decodeUser :: Value -> IO User
 decodeUser = withObject $ \obj ->
@@ -250,3 +319,23 @@ decodeUser = withObject $ \obj ->
     <*> atOrderedKey "description" text obj
     <*> atOrderedKey "url" (nullable text) obj
 
+twitterDecoder :: Monad f => D.Decoder f Twitter
+twitterDecoder =
+  Twitter
+    <$> D.atKey "statuses" (D.list statusDecoder)
+
+statusDecoder :: Monad f => D.Decoder f Status
+statusDecoder = D.withCursor $ \curs -> do
+  u       <- D.fromKey "user" userDecoder curs
+  newCurs <- D.moveToKey "metadata" curs
+  result  <- D.fromKey "result_type" D.text newCurs
+  iso     <- D.fromKey "iso_language_code" D.text newCurs
+  pure $ Status u result iso
+
+userDecoder :: Monad f => D.Decoder f User
+userDecoder =
+  User
+    <$> D.atKey "screen_name" D.text
+    <*> D.atKey "location" D.text
+    <*> D.atKey "description" D.text
+    <*> D.atKey "url" (D.maybeOrNull D.text)
